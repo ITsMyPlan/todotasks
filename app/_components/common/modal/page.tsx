e5777cbcde0213c2baba0fa7c7f482d30495dcd2
jieunw <jieunwang00@gmail.com>
2024-05-19 00:30:55 +0900

    setting: prettier 적용 오류 재수정

--- app/_components/common/modal/page.tsx
+++ app/_components/common/modal/page.tsx
@@ -1,10 +1,25 @@
-export default function openedModal() {
+import useModalStore from '../../../../store/store'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Modal = ({ children }: Props) => {
+  const { toggleModal } = useModalStore()
   return (
     <div>
-      <div>Task</div>
-      <div>x image</div>
-
-      <div>line</div>
+      <div className="modal-overlay" onClick={toggleModal}></div>
+      <div className="modal-content-container">
+        <div className="modal-content">
+          <h1>모달테스트</h1>
+          {children}
+          <button onClick={toggleModal} className="modal-close-button" type="button">
+            닫기
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Modal
