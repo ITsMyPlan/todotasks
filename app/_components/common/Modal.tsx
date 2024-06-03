a7d9534ce149d27f18d758f72bb35d5ad278550f
jieunw <jieunwang00@gmail.com>
2024-06-03 17:02:34 +0900

    feat: Modal 관련 기능 수정 및 추가
    * 데이터를 Form으로 제출하도록 변경
    * supabase에 데이터를 삽입하는 actions.ts 파일 추가

--- app/_components/common/Modal.tsx
+++ app/_components/common/Modal.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { createTask } from './actions'
 import useTaskStore from '@/store/useTaskStore'
 import { useAddTaskModalState, useViewTaskModalState, useModalActions } from '@/store/useModalStore'
 import { ModalProps } from '@/_types/modalType'
-import X from '@/public/icons/x.png'
-import AddIconWhite from '@/public/icons/whiteplus.png'
+import { useUserStore } from '@/store/useUserStore'
 
 import Image from 'next/image'
+import X from '@/public/icons/x.png'
+import AddIconWhite from '@/public/icons/whiteplus.png'
 
 const Modal = ({ children }: ModalProps) => {
   const addTaskOn = useAddTaskModalState()
@@ -17,11 +19,21 @@ const Modal = ({ children }: ModalProps) => {
   const [title, setTitle] = useState('')
   const [detail, setDetail] = useState('')
 
-  const handleAddTask = () => {
-    addTask(title, detail)
-    setTitle('')
-    setDetail('')
-    changeModalState('add')
+  const user = useUserStore(state => state.user)
+
+  // 현재 createTask에 changeModalState('add')라서 이름을 handleAddTask로 지어둠
+  const handleAddTask = (e: FormEvent) => {
+    if (!title) {
+      alert('please write a Task name')
+    } else if (title.length === 0) {
+      alert('please write a Task name at least 1 character')
+    } else {
+      e.preventDefault()
+      
+      createTask(e, title, detail, user, addTask, changeModalState)
+      setTitle('')
+      setDetail('')
+    }
   }
 
   if (!addTaskOn && !viewTaskOn) return null
@@ -38,39 +50,37 @@ const Modal = ({ children }: ModalProps) => {
 
         {addTaskOn && (
           <div className="">
-            <div className="">
-              <div className="border-b-4 border-lightGray/70 py-[8px]">
-                <input
-                  type="text"
-                  placeholder="Task"
-                  value={title}
-                  minLength={1}
-                  maxLength={50}
-                  onChange={e => setTitle(e.target.value)}
-                  className="w-full h-[30px] bg-transparent"
-                />
-              </div>
-              <div className="border-b-4 border-lightGray/70 py-[10px]">
-                <textarea
-                  placeholder="Description"
-                  value={detail}
-                  minLength={0}
-                  maxLength={150}
-                  onChange={e => setDetail(e.target.value)}
-                  className="outline-none resize-none w-full h-[25vh] bg-transparent"
-                />
-              </div>
-
-              <button
-                type="button"
-                onClick={handleAddTask}
-                className="absolute bottom-0 bg-gray-300 w-full h-[60px] rounded-xl"
-              >
-                <div className="flex justify-center items-center">
-                  <Image src={AddIconWhite} alt="addIcon" style={{ width: 20, height: 20 }} />
+            <form onSubmit={handleAddTask}>
+              <div className="">
+                <div className="border-b-4 border-lightGray/70 py-[8px]">
+                  <input
+                    type="text"
+                    placeholder="Task"
+                    value={title}
+                    minLength={1}
+                    maxLength={50}
+                    onChange={e => setTitle(e.target.value)}
+                    className="w-full h-[30px] bg-transparent focus:outline-none"
+                  />
                 </div>
-              </button>
-            </div>
+                <div className="border-b-4 border-lightGray/70 py-[10px]">
+                  <textarea
+                    placeholder="Description"
+                    value={detail}
+                    minLength={0}
+                    maxLength={150}
+                    onChange={e => setDetail(e.target.value)}
+                    className="outline-none resize-none w-full h-[25vh] bg-transparent"
+                  />
+                </div>
+
+                <button type="submit" className="absolute bottom-0 bg-gray-300 w-full h-[60px] rounded-xl">
+                  <div className="flex justify-center items-center">
+                    <Image src={AddIconWhite} alt="addIcon" style={{ width: 20, height: 20 }} />
+                  </div>
+                </button>
+              </div>
+            </form>
           </div>
         )}
         {viewTaskOn && <div>{children}</div>}
---
+++ app/_components/common/actions.ts
@@ -0,0 +1,35 @@
+import { createClient } from '@/_utils/supabase/client'
+import { User as SupabaseUser } from '@supabase/supabase-js'
+import { FormEvent } from 'react'
+
+interface User extends SupabaseUser {}
+// 사용자는 사용자 자신의 todolist만 CRUD할 수 있다는 정책을 걸어두면서 using auth.uid() = user_id라고 작성해서 user_id를 확인해야 하기 때문에 user:User 추가
+// userType.ts에 id 항목이 없어서 일단 타입을 supabase에서 받아왔는데 이후 수정할 수도 있음
+
+export const createTask = async (
+  e: FormEvent,
+  title: string,
+  detail: string,
+  user: User,
+  addTask: (title: string, detail: string) => void,
+  changeModalState: (type: 'add' | 'view') => void,
+) => {
+  const supabase = createClient()
+  if (!user) {
+    console.error('User is not authenticated!!')
+    return
+  }
+  const { data, error } = await supabase
+    .from('todolist')
+    .insert([{ todo_title: title, todo_detail: detail, user_id: user.id }])
+    .select()
+
+  if (error) {
+    alert('Error adding task: ' + error.message)
+  }
+  if (data) {
+    alert('Task added successfully!!!!')
+    addTask(title, detail)
+    changeModalState('add')
+  }
+}
