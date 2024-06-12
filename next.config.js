7336fdbe99dd12a6df3edce89f0370aed862c278
jieunw <jieunwang00@gmail.com>
2024-06-12 14:26:03 +0900

    chore: next.config.js에 절대 경로 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
-};
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app')
+    config.resolve.alias['@/store'] = path.resolve(__dirname, 'store')
+    config.resolve.alias['@/public'] = path.resolve(__dirname, 'public')
+    config.resolve.alias['@/styles'] = path.resolve(__dirname, 'styles')
+    config.resolve.alias['@/common'] = path.resolve(__dirname, 'common')
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
